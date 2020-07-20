0ca9cb4d7402a570c1cfa8dfa697da2dadcfea85
Paul Dhanda <pauldhanda@live.com>
2020-07-19 18:07:59 -0700

    setting more client structure

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
 // Import our outputted wasm ES6 module
 // Which, export default's, an initialization function
-import('./pkg')
-  .catch(console.error);
+// import('./pkg')
+//   .catch(console.error);
